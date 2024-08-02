c6e557270b93d84e897c761728b124caaab4c972
eugen <6739118@gmail.com>
2024-08-02 11:37:26 +0300

    Added empty project

---
+++ src/main/java/org/example/translator/TranslatorTestTaskApplication.java
@@ -0,0 +1,13 @@
+package org.example.translator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslatorTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TranslatorTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/translator/TranslatorTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.translator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslatorTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
