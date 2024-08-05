3425f3cee19214bb1b0a62ae82fd8a9bfdfa65bb
eugen <6739118@gmail.com>
2024-08-06 00:36:13 +0300

    Added some dependencies

--- src/main/java/org/example/translator/TranslatorTestTaskApplication.java
+++ src/main/java/org/example/translator/TranslatorTestTaskApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TranslatorTestTaskApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TranslatorTestTaskApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(TranslatorTestTaskApplication.class, args);
+  }
 }
